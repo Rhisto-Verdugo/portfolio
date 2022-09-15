5e1681ab2179b8e9cf813ce0deceb748d6997944
Rejvo <rjvomk2@gmail.com>
2022-09-15 01:29:31 -0600

    updated with newest project

--- components/Projects.js
+++ components/Projects.js
@@ -2,10 +2,12 @@ import Cloudy from "./Cloudy"
 import Luxerne from "./Luxerne"
 import Grill from "./Grill"
 import River from "./River"
+import Qxt from "./Qxt"
 
 const Projects = () => {
   return (
     <div>
+      <Qxt />
       <Cloudy />
       <Luxerne />
       <Grill />
